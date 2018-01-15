048497500cffb3602cec5b6b0a9dadd8e690f61a
Pablo Gonzalez <pablo@Pablos-Mac-mini.local>
2018-01-15 00:13:04 -0600

    Move POS logic to worker

--- www/static/js/pos.js
+++ www/static/js/pos.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         items: [],
         totalAmount: 0.0
     };
+    var posWorker = new Worker('js/posWorker.js');
 
     $('#code').keypress(function(e) {
         if(e.which === 13) {
@@ -21,21 +22,52 @@ $(document).ready(function() {
         alert('confirm');
     });
 
+    function removeItem(id) {
+        alert('remove ' + id);
+    }
+
     function addLineItem() {
         var code = $('#code').val();
         var qty = 1;
 
         if(code === '') return;
 
-        getProduct(code).then(function(product) {
-           alert(product);
-           ticket.items.push({
-               id: product.id,
-               desc: product.desc,
-               price: product.price,
-               qty: qty
-           });
+        posWorker.postMessage({
+            action: 'addItem',
+            code: code,
+            qty: qty
         });
+        getProduct(code).then(function(product) {
+           alert(product.id + ' ' + product.desc);
+           var item = {
+                id: product.id,
+                desc: product.desc,
+                price: product.price,
+                qty: qty
+            };
+            
+            ticket.items.push(item);
+            ticket.totalAmount += item.qty * item.price;
+            showLineItem(item);
+            showTotal();
+        });
+
+        clearAndFocus();
+    }
+
+    function showLineItem(item) {
+        var innerHtml = '<tr><td>' + item.id + '</td>'
+            + '<td>' + item.qty + '</td>'
+            + '<td>' + item.price + '</td>'
+            + '<td>' + item.desc + '</td>'
+            + '<td>&nbsp;</td>';
+            //+ '<td><button type="button" click="removeItem(' + item.id + ')">Delete</button></td>';
+
+        $('#detail tbody').append(innerHtml);
+    }
+
+    function showTotal() {
+        $('#total h3').text('$ ' + ticket.totalAmount);
     }
 
     function clearAndFocus() {
--- www/static/js/posWorker.js
+++ www/static/js/posWorker.js
@@ -1,3 +1,126 @@
+var ticket = {
+    items: [],
+    totalAmount: 0.0
+};
+
 self.addEventListener('message', function(e) {
-    
-})
+    var message = e.data;
+    console.log(message);
+
+    if(message.action === 'addItem') {
+        addItem(message.code, message.qty);
+    } else if(message.action === 'cancel') {
+        clearTicket();
+    } else if(message.action === 'saveTicket') {
+        saveTicket();
+    }
+}, false);
+
+function addItem(productCode, qty) {
+    getJson('/api/products/' + productCode, function(product) {
+        var item = {
+            id: product.id,
+            desc: product.desc,
+            price: product.price,
+            qty: qty
+        };
+        
+        ticket.items.push(item);
+        ticket.totalAmount += item.qty * item.price;
+
+        self.postMessage({
+            action: 'addItem',
+            success: true,
+            item: item,
+            totalAmount: ticket.totalAmount
+        });
+    }, function(err) {
+        self.postMessage({
+            action: 'addItem',
+            success: false,
+            message: err.status + ': ' + err.response
+        });
+    });
+}
+
+function clearTicket() {
+    resetTicket();
+
+    self.postMessage({
+        action: 'cancel',
+        success: true
+    });
+}
+
+function saveTicket() {
+    postJson('/api/sales', ticket, function() {
+        resetTicket();
+
+        self.postMessage({
+            action: 'saveTicket',
+            success: true
+        });
+    }, function(err) {
+        self.postMessage({
+            action: 'saveTicket',
+            success: false,
+            message: err.status + ': ' + err.response
+        });
+    });
+}
+
+function resetTicket() {
+    ticket.items = [];
+    ticket.totalAmount = 0.0;
+}
+
+function getJson(url, success, error) {
+    var xhr = getXmlHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState < 4) return;
+        if(xhr.status !== 200) {
+            error({
+                status: xhr.status,
+                response: xhr.responseText
+            });
+        }
+        
+        var json = parseJson(xhr.responseText);
+        success(json);
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send('');
+}
+
+function postJson(url, obj, success, error) {
+    var xhr = getXmlHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState < 4) return;
+        if(xhr.status >= 400 && xhr.status <= 503) {
+            error({
+                status: xhr.status,
+                response = xhr.responseText
+            });
+        }
+
+        success();
+    }
+
+    xhr.open('POST', url, true);
+    xhr.send(obj);
+}
+
+function getXmlHttpRequest() {
+    var xhr;
+    if(typeof XMLHttpRequest !== 'undefined') {
+        xhr = new XMLHttpRequest();
+    }
+    return xhr;
+}
+
+function parseJson(text) {
+    return JSON.parse(text);
+}
